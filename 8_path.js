045c4ba03991c9038358dd44b00927660680ff10
gkrud <gkrud823@naver.com>
2025-04-22 14:18:00 +0900

    first commit

---
+++ 8_path.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const skills = [
+	{name: 'HTML'}
+	{name: 'CSS'}
+	{name: 'JAVASCRIPT'}
+	{name: 'JAVA'}
+	{name: 'PYTHON'}
+	{name: 'AI'}
+	{name: 'Node.js'}
+	{name: 'mongoDB'}
+	{name: 'MySQL'}
+]
+
+const server = http.createServer((req, res) => {
+	
+})
